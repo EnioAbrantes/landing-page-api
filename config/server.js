cd3f9dc6b5bb7869dda5051ebd7d27e58f460a5f
Ênio <enio.abrantes.silva@gmail.com>
2021-05-10 12:42:51 -0300

    add logo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a4cc2304268ec86d9fe700c52771bf96'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5cf0db5b-5c1c-429f-8daa-c9996febe06f'
+};
